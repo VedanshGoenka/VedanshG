8becdbca9bc1eb814122245c9bbcf4e254137ac3
VedanshGoenka <vedansh@vgoenka.com>
2021-01-05 23:09:14 -0800

    Added unstyled Read More Button
    Put articles in article tags.
    Reformatted a lil' bit.
    Added js :smiley:

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,21 @@
 function toggle(visible) {
-    document.querySelectorAll(visible).forEach(e => { e.hidden = !e.hidden;});
+  document.querySelectorAll(visible).forEach((e) => {
+    e.hidden = !e.hidden;
+  });
+}
+
+function showMoreLess() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("clickButton");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
 }
