769808a6aa99db5fdaa30fa3ff504ead25f9aa72
VedanshGoenka <vedansh@vgoenka.com>
2021-01-08 19:59:05 -0800

    Split Buttons; Added Fiber Blog

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,11 +5,21 @@ function toggle(visible) {
   });
 }
 
-// Read More/Less Toggle
-function showMoreLess() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("clickButton");
+// Reduces the size of index.html
+$(function () {
+  $("#hobbies-html").load("hobbies.html");
+  $("#aboutme-html").load("aboutme.html");
+  $("#astrophotography2020-article").load("/assets/blog/astrophotography2020/");
+  $("#fiberopticcable-article").load("/assets/blog/fiberopticinstall/");
+  $("#contact-html").load("contact.html");
+});
+
+
+// Read More/Less Toggles
+function showMoreLessAstro() {
+  var dots = document.getElementById("dots-astro2020");
+  var moreText = document.getElementById("more-astro2020");
+  var btnText = document.getElementById("clickButton-astro2020");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline";
@@ -22,10 +32,23 @@ function showMoreLess() {
   }
 }
 
-// Reduces the size of index.html
-$(function () {
-  $("#hobbies-html").load("hobbies.html");
-  $("#aboutme-html").load("aboutme.html");
-  $("#astrophotography2020-article").load("/assets/blog/astrophotography2020/astrophoto2020.html");
-  $("#contact-html").load("contact.html");
-});
+function showMoreLessFiber() {
+  var dots = document.getElementById("dots-fiber");
+  var moreText = document.getElementById("more-fiber");
+  var btnText = document.getElementById("clickButton-fiber");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read More";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read Less";
+    moreText.style.display = "inline";
+  }
+}
+
+
+
+
+
